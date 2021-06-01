c51b44f15387f1f5a2e83ec795f30a573ad21058
Kusakina <dashakys01@gmail.com>
2021-06-02 03:59:46 +0400

    added log

--- src/SmokerThread.java
+++ src/SmokerThread.java
@@ -1,5 +1,11 @@
 import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public class SmokerThread extends Thread {
@@ -24,41 +30,103 @@ public class SmokerThread extends Thread {
         }
         id = count++;
         System.out.println("Smoker"+id+" created he has " + typeRecorder.getSmokerThings(necessaryObjectType));
-        try {
-            this.out.write("Smoker" + id + " created, he has " + typeRecorder.getSmokerThings(necessaryObjectType) + "\n");
-            this.out.flush();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        try (FileOutputStream fileOutputStream = new FileOutputStream("log.txt");
+             FileChannel fileChannel = fileOutputStream.getChannel();
+             PrintWriter out2 = new PrintWriter(fileOutputStream)) {
+            try {
+                this.out.write("Smoker" + id + " created, he has " + typeRecorder.getSmokerThings(necessaryObjectType) + "\n");
+                this.out.flush();
+                while (true) {
+                    try (FileLock lock = fileChannel.tryLock()) {
+                        if (null == lock) continue;
+
+                        LocalDateTime dateTime = LocalDateTime.now();
+                        String logDatePattern = "dd.MM.yyyy HH:mm:ss";
+                        DateTimeFormatter logDateFormatter = DateTimeFormatter.ofPattern(logDatePattern);
+                        out2.print(logDateFormatter.format(dateTime) + ": " + "New Object was given! On table "+ typeRecorder.getDealerThings(object.getType())+"\n" );
+                        break;
+                    }
+                }
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        } catch (IOException e) {
+        e.printStackTrace();
     }
 
     public void run(){
         while (true){
             object.acquire();
-            if(object.getType()==necessaryObjectType){
-                object.setType(0);
-                try {
-                    out.write("Smoker "+id + " Oyyy, I need it, because I have "+ typeRecorder.getSmokerThings(necessaryObjectType) + "\n");
-                    out.flush();
-                    out.write("Smoker "+id + " is smoking\n");
-                    out.flush();
-                    System.out.println("Smoker "+id + " is smoking");
-                    sleep(SMOKING_TIME);
-                    out.write("Smoker "+id + " stopped smoking\n");
-                    out.flush();
+            try (FileOutputStream fileOutputStream = new FileOutputStream("log.txt");
+                 FileChannel fileChannel = fileOutputStream.getChannel();
+                 PrintWriter out2 = new PrintWriter(fileOutputStream)) {
+                if(object.getType()==necessaryObjectType){
+                    object.setType(0);
+                    try {
+                        out.write("Smoker "+id + " Oyyy, I need it, because I have "+ typeRecorder.getSmokerThings(necessaryObjectType) + "\n");
+                        out.flush();
+                        out.write("Smoker "+id + " is smoking\n");
+                        out.flush();
+
+                        while (true) {
+                            try (FileLock lock = fileChannel.tryLock()) {
+                                if (null == lock) continue;
+
+                                LocalDateTime dateTime = LocalDateTime.now();
+                                String logDatePattern = "dd.MM.yyyy HH:mm:ss";
+                                DateTimeFormatter logDateFormatter = DateTimeFormatter.ofPattern(logDatePattern);
+                                out2.print(logDateFormatter.format(dateTime) + ": " + "Smoker "+id + " is smoking\n");
+                                break;
+                            }
+                        }
+
+                        System.out.println("Smoker "+id + " is smoking");
+                        sleep(SMOKING_TIME);
+                        out.write("Smoker "+id + " stopped smoking\n");
+                        out.flush();
+
+                        while (true) {
+                            try (FileLock lock = fileChannel.tryLock()) {
+                                if (null == lock) continue;
+
+                                LocalDateTime dateTime = LocalDateTime.now();
+                                String logDatePattern = "dd.MM.yyyy HH:mm:ss";
+                                DateTimeFormatter logDateFormatter = DateTimeFormatter.ofPattern(logDatePattern);
+                                out2.print(logDateFormatter.format(dateTime) + ": " + "Smoker "+id + " stopped smoking\n");
+                                break;
+                            }
+                        }
+
+                        object.release();
+                        sleep(SLEEP_TIME);
+
+                        while (true) {
+                            try (FileLock lock = fileChannel.tryLock()) {
+                                if (null == lock) continue;
+
+                                LocalDateTime dateTime = LocalDateTime.now();
+                                String logDatePattern = "dd.MM.yyyy HH:mm:ss";
+                                DateTimeFormatter logDateFormatter = DateTimeFormatter.ofPattern(logDatePattern);
+                                out2.print(logDateFormatter.format(dateTime) + ": " + "Smoker "+id + " is sleeping \n" );
+                                break;
+                            }
+                        }
+                        //out.write("Smoker "+id + " waiting for "+typeRecorder.getDealerThings(necessaryObjectType)+"\n");
+                        //out.flush();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        object.release();
+                        break;
+                    }
+                }else {
                     object.release();
-                    sleep(SLEEP_TIME);
-                    //out.write("Smoker "+id + " waiting for "+typeRecorder.getDealerThings(necessaryObjectType)+"\n");
-                    //out.flush();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    object.release();
-                    break;
                 }
-            }else {
-                object.release();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
 
     }
-}
+
